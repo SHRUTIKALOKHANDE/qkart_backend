1a64ffc30d198cd5ddde5deea6b8619ebadc8679
CrioUser <crio-user@criodo.com>
2021-04-14 19:41:27 +0530

    ME_QKART_BACKEND_MODULE_UNDERSTANDING_BASICS

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -41,7 +41,8 @@ const userSchema = mongoose.Schema(
     },
     address: {
       type: String,
-      default: config.default_address,
+      required: true,
+      //default: config.default_address,
     },
   },
   // Create createdAt and updatedAt fields automatically
