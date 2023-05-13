5027e0403054823e4a0dd78c49777c4050c847b0
SHRUTIKALOKHANDE <shrutikalokhande449@gmail.com>
2023-05-13 17:59:03 +0530

    Update setupRemoteMongo.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,2 +1,5 @@
-mongoimport --uri 'mongodb+srv://admin:admin123@qkart-node.zzbn7.mongodb.net/qkart?retryWrites=true&w=majority' --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri 'mongodb+srv://admin:admin123@qkart-node.zzbn7.mongodb.net/qkart?retryWrites=true&w=majority' --drop --collection products --file data/export_qkart_products.json
+# mongoimport --uri 'mongodb+srv://admin:admin123@qkart-node.zzbn7.mongodb.net/qkart?retryWrites=true&w=majority' --drop --collection users --file data/export_qkart_users.json
+# mongoimport --uri 'mongodb+srv://admin:admin123@qkart-node.zzbn7.mongodb.net/qkart?retryWrites=true&w=majority' --drop --collection products --file data/export_qkart_products.json
+
+mongoimport --uri 'mongodb+srv://shrutikalokhande449:7fVVWfF9Zhuxi4tj@cluster0.m51yeca.mongodb.net/?retryWrites=true&w=majority' --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri 'mongodb+srv://shrutikalokhande449:7fVVWfF9Zhuxi4tj@cluster0.m51yeca.mongodb.net/?retryWrites=true&w=majority' --drop --collection products --file data/export_qkart_products.json
